850e5b93f6478f71870063df7854e6ab272b607c
Dimitris Kottas <dkottas1973@gmail.com>
2024-06-12 13:59:14 +0300

    build without pages

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,33 +3,33 @@ import type { NextRequest } from 'next/server';
 import { i18n } from '@/config/languages-config';
 
 export function middleware(request: NextRequest) {
-    // const pathname = request.nextUrl.pathname;
+    const pathname = request.nextUrl.pathname;
 
-    // let newPathname = pathname;
+    let newPathname = pathname;
 
-    // if (!newPathname.startsWith('/')) {
-    //     newPathname = `/${newPathname}`;
-    // }
+    if (!newPathname.startsWith('/')) {
+        newPathname = `/${newPathname}`;
+    }
 
-    // const pathsegments = newPathname.split('/');
+    const pathsegments = newPathname.split('/');
 
-    // if (pathsegments.length === 1) {
-    //     newPathname = `${newPathname}/en`;
+    if (pathsegments.length === 1) {
+        newPathname = `${newPathname}/en`;
 
-    // } else if (!i18n.locales.includes(pathsegments[1] as 'en' | 'es')) {
-    //     newPathname = `/en${newPathname}`;
-    // }
+    } else if (!i18n.locales.includes(pathsegments[1] as 'en' | 'es')) {
+        newPathname = `/en${newPathname}`;
+    }
 
-    // if (newPathname != pathname) {
-    //     return NextResponse.redirect(
-    //         new URL(
-    //             newPathname,
-    //             request.url,
-    //         ),
-    //     );
-    // }
+    if (newPathname != pathname) {
+        return NextResponse.redirect(
+            new URL(
+                newPathname,
+                request.url,
+            ),
+        );
+    }
 }
 
-// export const config = {
-//     matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
-// };
+export const config = {
+    matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+};
